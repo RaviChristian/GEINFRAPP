7251aed3e1223fafd9af3cae517c6aae88087d15
Ravi Christian <108839457+RaviChristian@users.noreply.github.com>
2023-05-26 16:52:14 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
-import HomeButton from './HomeButton';
-import Navigation from './Navigation';
-import {AiOutlineMenu} from 'react-icons/ai'
-import Section from './section';
-import Footer from './Footer';
-
+import RenderedForm from './RenderedForm';
 import './App.css';
 
 function App() {
 
   return(
     <>
-      <Navigation/>
-      <Section/>
-      <Footer/>
+      <RenderedForm/>
     </>
   );
 }
