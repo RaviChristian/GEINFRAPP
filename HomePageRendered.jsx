cfe2588c0211992e9e52aec1a32c4b3ff533831d
Ravi Christian <108839457+RaviChristian@users.noreply.github.com>
2023-05-28 14:16:45 -0300

    Add files via upload

---
+++ HomePageRendered.jsx
@@ -0,0 +1,17 @@
+import Navigation from './Navigation';
+import Section from './section';
+import Footer from './Footer';
+
+
+function HomePageRendered() {
+
+  return(
+    <>
+      <Navigation/>
+      <Section/>
+      <Footer/>
+    </>
+  );
+}
+
+export default HomePageRendered
