437093f434253ecb0c9b9300a35716fc59ede599
Ravi Christian <108839457+RaviChristian@users.noreply.github.com>
2023-05-28 14:52:48 -0300

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import HomePageRendered from './HomePageRendered.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App/>
+    <HomePageRendered/>
 
   </React.StrictMode>,
 )
